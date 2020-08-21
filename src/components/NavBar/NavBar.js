69768f78a113ed9165cdfd0b7f987e1abb852a50
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-21 19:09:01 +0100

    Navbar now implemented with Material-UI tabs. Looks nice :)

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./NavBar.css";
 import routes from '../../data/routes'
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Paper from '@material-ui/core/Paper';
 
-function showRoutes() {
-    return routes.map((route, index) =>
-        <li key={index}>
-            <Link to={route.path}>{route.name}</Link>
-        </li>
 
-    )
-}
+
+
 function NavBar() {
+
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    function showRoutes() {
+        return routes.map((route, index) =>
+            <Tab key={index} label={route.name} component={Link} to={route.path} />
+        )
+    }
+
     return (
-        <ul className="nav-list">
-            {showRoutes()}
-        </ul>
+        <Paper elevation={1}>
+            <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                centered>
+                {showRoutes()}
+            </Tabs>
+        </Paper>
     )
 }
 
