623a0dd04a81f4a0e2663f9c46d8d53d418ead98
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-21 23:25:38 +0100

    Flex columns to main layout. Now we have two clear sections.

--- src/App.js
+++ src/App.js
@@ -13,21 +13,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Container>
-        <NavBar />
-        <Switch>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              component={route.component}
-              exact={route.exact}
-            />
-          ))}
-        </Switch>
-      </Container>
+      <NavBar />
+      <div className="row">
+        <div className="column left">
+          <h1>Profile pic / etc here</h1>
+        </div>
+
+        <div className="column right">
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                component={route.component}
+                exact={route.exact}
+              />
+            ))}
+          </Switch>
+        </div>
+      </div>
       <Footer />
-    </div>
+
+
+    </div >
+
+
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Projects(){
-    return <h1>Contact Me</h1>
+    return <h1>Projects</h1>
 }
 
 export default Projects
