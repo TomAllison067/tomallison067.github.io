528094c8c24fd520e52332f3f930a87134590ad4
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-22 17:53:24 +0100

    Fixed NavBar navigation highlighting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,17 +11,28 @@ import './styles/navbar.css';
 
 
 function NavBar(props) {
-    console.log(props);
-    const [value, setValue] = React.useState(0);
-
+    // Set the highlighted tab to the current page - eg, if you refresh.
+    const [value, setValue] = React.useState(() => {
+        let url = window.location.href.split('/').pop();
+        let initialValue;
+        switch(url){
+            case "":
+                initialValue = 0;
+                break;
+            case "interests":
+                initialValue = 1;
+                break;
+            default:
+                initialValue = 2;
+        }
+        return initialValue;
+    });
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-
     function showRoutes() {
         return routes.filter((route) => route.used).map((route, index) => <Tab key={index} label={route.name} component={Link} to={route.path} />)
     }
-
     return (
         <Paper className="tab-colouring" elevation={1}>
             <Tabs className="tab-colouring"
