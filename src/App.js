9ad461e1d730a89b644c7fb0246b19b46ad1e51e
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-22 11:04:00 +0100

    Removed unused dark themes

--- src/App.js
+++ src/App.js
@@ -19,21 +19,7 @@ function App() {
       background: {
         default: "#fafafa00"
       }
-    },
-    typography: {
-      variantMapping: {
-        h1: 'h2',
-        h2: 'h2',
-        h3: 'h2',
-        h4: 'h2',
-        h5: 'h2',
-        h6: 'h2',
-        subtitle1: 'h2',
-        subtitle2: 'h2',
-        body1: 'span',
-        body2: 'span',
-      },
-    },
+    }
   });
   themeLight = responsiveFontSizes(themeLight);
   return (
