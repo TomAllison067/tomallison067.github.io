72920791bb817d93d261289c79c6d87e73b9f642
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-22 16:51:11 +0100

    Placeholder

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
 
-function Contact(){
-    return <h1>Contact Me</h1>
+function Contact() {
+    return (
+        <div>
+            <Typography component="h2" variant="h4">Contact me</Typography>
+            <Typography component="p" variant="p">WIP</Typography>
+        </div>
+    );
 }
 
 export default Contact
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -8,8 +8,9 @@ function Interests() {
     return (
         <div>
             <Typography component="h2" variant="h4">Things I Like</Typography>
-            <TwoColumnSection type="left-heavy" left={<Article content="foo" />} right={<SpotifyFrame />} />
-            <TwoColumnSection type="right-heavy" left={<Article content="bar" />} right={<Article content="baz" />} />
+            <TwoColumnSection type="left-heavy" left={<Article content="Please bare with me - this site is only 1 day old. In the mean time,
+            here's some music you might like." />} right={<SpotifyFrame />} />
+            <TwoColumnSection type="right-heavy" left={<Article content="" />} right={<Article content="" />} />
         </div>
     );
 }
