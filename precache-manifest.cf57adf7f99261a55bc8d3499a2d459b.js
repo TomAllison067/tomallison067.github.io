10d4cf4c10ce9fb8be2e36161aeb088e4baff3fa
TomAllison067 <tomlaurenceallison@gmail.com>
2020-08-22 14:19:28 +0100

    Updates

---
+++ precache-manifest.cf57adf7f99261a55bc8d3499a2d459b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe31befa1e629580a9049f171b2464bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d16ec36c7013ca9bc90",
+    "url": "/static/css/main.f06e0c48.chunk.css"
+  },
+  {
+    "revision": "bba38e6735d4e8739d08",
+    "url": "/static/js/2.6f591b07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6f591b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d16ec36c7013ca9bc90",
+    "url": "/static/js/main.122e4e67.chunk.js"
+  },
+  {
+    "revision": "f4800b06d7eae560929f",
+    "url": "/static/js/runtime-main.c72e126b.js"
+  },
+  {
+    "revision": "df28ee47227f60e28e86649bfe8fa798",
+    "url": "/static/media/profile.df28ee47.png"
+  }
+]);
--- src/components/ProfilePic.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import profile from '../assets/profile.png';
-import './styles/profilepic.css';
-
-function ProfilePic(){
-    return <img src={profile} className="profile-pic" alt="Enjoying nature" />
-}
-
-export default ProfilePic
